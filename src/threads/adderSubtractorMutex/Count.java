f9246f420101cc2b34dca6d6c70bf9f16e50a302
Alok Singh <singhalok641@gmail.com>
2024-03-07 00:01:46 +0100

    mutex

---
+++ src/threads/adderSubtractorMutex/Count.java
@@ -0,0 +1,5 @@
+package threads.adderSubtractorMutex;
+
+public class Count {
+    int value;
+}
---
+++ src/threads/adderSubtractorMutex/Subtractor.java
@@ -0,0 +1,24 @@
+package threads.adderSubtractorMutex;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Subtractor implements Runnable{
+
+    private Count count;
+    private Lock lock;
+
+    public Subtractor(Count count, Lock lock){
+        this.count = count;
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+        for(int i=1;i<=10000;i++){
+            lock.lock();
+            count.value -= i;
+            lock.unlock();
+        }
+    }
+}
